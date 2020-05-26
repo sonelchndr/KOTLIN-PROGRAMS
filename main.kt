c6dd25e15adbc8e784d9a2c6ad497048a00a9a13
Sonel Chandra <sonelchandra4@gmail.com>
2020-05-26 15:36:16 +0530

    Main File

---
+++ main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val robot1=Robot("RA ONE")
+    println("Hey i am ${robot1.name} created by sonel chandra , How may i help you? ")
+    robot1.RingAlarm("8AM")
+
+
+    robot1.MakeCoffee("black",2)
+    robot1.HeatWater(30)
+    robot1.PackBag("monday")
+    robot1.CookBreakfast()
+    robot1.CookLunch()
+    robot1.IronClothes(3)
+}
